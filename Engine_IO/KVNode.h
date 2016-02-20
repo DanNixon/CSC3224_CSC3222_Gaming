8ab0e45666d73e434ea34637d8778bc8fa3c05fc
Dan Nixon <dan@dan-nixon.com>
2016-02-20 21:46:13 +0000

    Add remaining docs

--- Engine_IO/KVNode.h
+++ Engine_IO/KVNode.h
@@ -18,6 +18,12 @@
 class KVNode
 {
 public:
+  /**
+   * @brief Creates a new node.
+   * @param name Name of this node (defaults to an empty name)
+   *
+   * Root nodes typically have an empty name.
+   */
   KVNode(const std::string &name = "")
       : m_name(name)
   {
@@ -29,60 +35,124 @@ public:
     m_children.clear();
   }
 
+  /**
+   * @brief Returns the name of this node.
+   * @return Node name
+   */
   std::string name() const
   {
     return m_name;
   }
 
+  /** @name Key-Value pair functions
+   *  @{
+   */
+
+  /**
+   * @brief Returns the number of key-value pairs in this node.
+   * @return Number of key-value pairs
+   */
   size_t numKeys() const
   {
     return m_keys.size();
   }
 
+  /**
+   * @brief Checks to see if a key exists in this node.
+   * @param key Key to test for
+   * @return True if key exists
+   */
   bool hasKey(const std::string &key) const
   {
     return (m_keys.find(key) != m_keys.end());
   }
 
+  /**
+   * @brief Gets the value of a key.
+   * @param key Key to retrieve
+   * @return Value
+   */
   std::string get(const std::string &key)
   {
     return m_keys[key];
   }
 
+  /**
+   * @brief Sets the value of a key.
+   * @param key Key to set
+   * @param value Value
+   */
   void set(const std::string &key, const std::string &value)
   {
     m_keys[key] = value;
   }
 
+  /**
+   * @brief Removes a key from the node.
+   * @param key Key to remove
+   * @return True if key was successfully removed
+   */
   bool remove(const std::string &key)
   {
     return (m_keys.erase(key) == 1);
   }
 
+  /** @} */
+
+  /** @name Child node functions
+   *  @{
+   */
+
+  /**
+   * @brief Returns the number of child nodes of this node.
+   * @return Number of child nodes
+   */
   size_t numChildren() const
   {
     return m_children.size();
   }
 
+  /**
+   * @brief Checks to see if a child node with a given name exists in this
+   *        node.
+   * @param name Name of child node to test for
+   * @return True if child exists
+   */
   bool hasChild(const std::string &name) const
   {
     return (m_children.find(name) != m_children.end());
   }
 
+  /**
+   * @brief Gets a child node with a given name.
+   * @param name Name of child not to retrieve
+   * @return Child node
+   */
   KVNode child(const std::string &name)
   {
     return m_children[name];
   }
 
+  /**
+   * @brief Adds a child node to this node..
+   * @param node Child node to add
+   */
   void addChild(KVNode node)
   {
     m_children[node.name()] = node;
   }
 
+  /**
+   * @brief Removes a child node from the node.
+   * @param name Name of child to remove
+   * @return True if child was successfully removed
+   */
   bool removeChild(const std::string &name)
   {
     return (m_children.erase(name) == 1);
   }
+
+  /** @} */
 
 private:
   friend class INIKeyValueStore;
