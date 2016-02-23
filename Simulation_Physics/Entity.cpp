cbfa6106b42c4d001ed5dc2f870c48a99312fddd
Dan Nixon <dan@dan-nixon.com>
2016-02-23 13:27:39 +0000

    add vel/acc getters and seters

--- Simulation_Physics/Entity.cpp
+++ Simulation_Physics/Entity.cpp
@@ -45,11 +45,43 @@ namespace Physics
   /**
    * @brief Sets the position of this Entity.
    * @param pos Position
-   * @see Entity::position
+   * @see Entity::shiftPosition()
+   * @see Entity::position()
    */
   void Entity::setPosition(const Vector2 &pos)
   {
     m_position = pos;
+  }
+
+  /**
+   * @brief Offsets the position of this Entity by a given amount.
+   * @param offset Position offset
+   * @see Entity::setPosition()
+   * @see Entity::position()
+   */
+  void Entity::shiftPosition(const Vector2 &offset)
+  {
+    m_position += offset;
+  }
+
+  /**
+   * @brief Sets the velocity of this Entity in the current timestep.
+   * @param vel Velocity
+   * @see Entity::velocity()
+   */
+  void Entity::setVelocity(const Vector2 &vel)
+  {
+    m_velocity = vel;
+  }
+
+  /**
+   * @brief Sets the acceleration of this Entity in the current timestep.
+   * @param acc Acceleration
+   * @see Entity::acceleration()
+   */
+  void Entity::setAcceleration(const Vector2 &acc)
+  {
+    m_acceleration = acc;
   }
 }
 }
--- Simulation_Physics/Entity.h
+++ Simulation_Physics/Entity.h
@@ -28,7 +28,8 @@ namespace Physics
     /**
      * @brief Gets the position of this entity.
      * @return Position
-     * @see Entity::setPosition
+     * @see Entity::setPosition()
+     * @see Entity::shiftPosition()
      */
     inline Engine::Maths::Vector2 position() const
     {
@@ -36,9 +37,36 @@ namespace Physics
     }
 
     virtual void setPosition(const Engine::Maths::Vector2 &pos);
+    virtual void shiftPosition(const Engine::Maths::Vector2 &offset);
+
+    /**
+     * @brief Gets the velocity of this entity in the current timestep.
+     * @return Velocity
+     * @see Entity::setVelocity()
+     */
+    inline Engine::Maths::Vector2 velocity() const
+    {
+      return m_velocity;
+    }
+
+    void setVelocity(const Engine::Maths::Vector2 &vel);
+
+    /**
+     * @brief Gets the acceleration of this entity in the current timestep.
+     * @return Acceleration
+     * @see Entity::setAcceleration()
+     */
+    inline Engine::Maths::Vector2 acceleration() const
+    {
+      return m_acceleration;
+    }
+
+    void setAcceleration(const Engine::Maths::Vector2 &acc);
 
   private:
     Engine::Maths::Vector2 m_position; //!< Position of Entity
+    Engine::Maths::Vector2 m_velocity; //!< Velocity in current timestep
+    Engine::Maths::Vector2 m_acceleration; //!< Acceleration in current timestep
   };
 }
 }
