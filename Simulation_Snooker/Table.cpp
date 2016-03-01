e4b65a036a930e2a87293ef63b9dd440532215f1
Dan Nixon <dan@dan-nixon.com>
2016-03-01 12:49:46 +0000

    correct dims

--- Simulation_Snooker/Table.cpp
+++ Simulation_Snooker/Table.cpp
@@ -15,12 +15,22 @@ using namespace Engine::Graphics;
 /**
  * @brief Dimensions of the table in mm.
  */
-const Vector2 Table::DIMENSIONS = Vector2(3569.0f, 1778.0f);
+const Vector2 Table::DIMENSIONS = Vector2(3880.0f, 2080.0f);
 
 /**
  * @brief Half the dimensions of the table in mm.
  */
 const Vector2 Table::HALF_DIMENSIONS = DIMENSIONS / 2;
+
+/**
+ * @brief Dimensions of the play area in mm.
+ */
+const Vector2 Table::PLAY_AREA = Vector2(3568.7f, 1778.0f);
+
+/**
+ * @brief Half the dimensions of the play area in mm.
+ */
+const Vector2 Table::HALF_PLAY_AREA = PLAY_AREA / 2;
 
 /**
  * @brief Creates a new table.
@@ -38,10 +48,10 @@ Table::Table()
   tex->load("table.bmp");
   setTexture(tex);
 
-  m_cushions[0] = new Cushion(Vector2(-HALF_DIMENSIONS.x(), 0.0f));
-  m_cushions[1] = new Cushion(Vector2(HALF_DIMENSIONS.x(), 0.0f));
-  m_cushions[2] = new Cushion(Vector2(0.0f, -HALF_DIMENSIONS.y()));
-  m_cushions[3] = new Cushion(Vector2(0.0f, HALF_DIMENSIONS.y()));
+  m_cushions[0] = new Cushion(Vector2(-HALF_PLAY_AREA.x(), 0.0f));
+  m_cushions[1] = new Cushion(Vector2(HALF_PLAY_AREA.x(), 0.0f));
+  m_cushions[2] = new Cushion(Vector2(0.0f, -HALF_PLAY_AREA.y()));
+  m_cushions[3] = new Cushion(Vector2(0.0f, HALF_PLAY_AREA.y()));
 }
 
 Table::~Table()
