ba05bd6ab1bd0997b66963cd2a6d895710067971
Dan Nixon <dan@dan-nixon.com>
2016-03-05 12:10:41 +0000

    move resources for demo

--- GameDev_Demo/DemoGame.cpp
+++ GameDev_Demo/DemoGame.cpp
@@ -36,17 +36,17 @@ DemoGame::~DemoGame()
 void DemoGame::gameStartup()
 {
   m_sp = new ShaderProgram();
-  m_sp->addShader(new VertexShader("basic_vertex.glsl"));
-  m_sp->addShader(new FragmentShader("basic_fragment.glsl"));
+  m_sp->addShader(new VertexShader("../resources/shader/vert_simple.glsl"));
+  m_sp->addShader(new FragmentShader("../resources/shader/frag_col.glsl"));
   m_sp->link();
 
   m_cube =
-      new RenderableObject(Mesh::LoadASCIIMeshFile("cube.asciimesh"), m_sp);
+      new RenderableObject(Mesh::LoadASCIIMeshFile("../resources/cube.asciimesh"), m_sp);
   m_cube->setModelMatrix(Matrix4::Translation(Vector3(0.0, 0.0, -10.0)) *
                          Matrix4::Rotation(45, Vector3(0, 1, 0)) *
                          Matrix4::Rotation(45, Vector3(1, 0, 0)));
 
-  m_child = new RenderableObject(Mesh::LoadModelFile("sphere.stl", 0), m_sp);
+  m_child = new RenderableObject(Mesh::LoadModelFile("../resources/sphere.stl", 0), m_sp);
   m_child->setModelMatrix(Matrix4::Translation(Vector3(-2.0, 0.0, 0.0)) *
                           Matrix4::Rotation(30, Vector3(1, 0, 0)));
 
