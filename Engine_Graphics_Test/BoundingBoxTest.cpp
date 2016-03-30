8154d103c9c9c0422736fee436bf9074f9119e86
Dan Nixon <dan@dan-nixon.com>
2016-03-30 23:48:40 +0100

    add emty graphics test lib and boundingbox test case

---
+++ Engine_Graphics_Test/BoundingBoxTest.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * @author Dan Nixon
+ */
+
+#include <CppUnitTest.h>
+
+#include <Engine_Graphics/BoundingBox.h>
+#include <Engine_Maths/Vector3.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Engine::Maths;
+
+namespace Engine
+{
+namespace Graphics
+{
+namespace Test
+{
+#ifndef DOXYGEN_SKIP
+TEST_CLASS(BoundingBoxTest)
+{
+public:
+  TEST_METHOD(BoundingBox_InitEmpty)
+  {
+    BoundingBox<Vector3> b;
+  }
+
+  // TODO
+};
+#endif /* DOXYGEN_SKIP */
+}
+}
+}
