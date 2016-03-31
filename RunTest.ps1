c7ac310471614ba2e2c4aff63a6dbf27f091e43f
Dan Nixon <dan@dan-nixon.com>
2016-03-31 08:49:51 +0100

    Refactor build for testing

---
+++ RunTest.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+Runs a unit test case.
+.PARAMETER TestName
+Name of the test suite to run.
+.EXAMPLE
+RunTest -TestName Engine_Maths_Test
+#>
+
+param(
+  [Parameter(Mandatory=$true)][string]$TestName
+)
+
+$platform_map=@{"Win32" = "x86"; "X64" = "x64"}
+$test_platform = $platform_map.$Env:PLATFORM
+
+vstest.console `
+    /logger:Appveyor `
+    /inIsolation `
+    /Platform:$test_platform `
+    .\bin\$TestName\$Env:PLATFORM\$Env:CONFIGURATION\$TestName.dll
