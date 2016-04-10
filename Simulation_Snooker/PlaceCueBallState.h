2abc75c25eba93ca16e47bfb945954e5e9e610b7
Dan Nixon <dan@dan-nixon.com>
2016-04-11 00:47:27 +0100

    refactor
    Re #14

--- Simulation_Snooker/PlaceCueBallState.h
+++ Simulation_Snooker/PlaceCueBallState.h
@@ -26,70 +26,13 @@ namespace Snooker
   {
   public:
     PlaceCueBallState(Simulation::AI::IState *parent, Simulation::AI::StateMachine *machine,
-                      SnookerSimulation *simulation)
-        : CompletableActionState("place_cue_ball", parent, machine)
-        , m_simulation(simulation)
-    {
-    }
-
-    ~PlaceCueBallState()
-    {
-      resetMouseStartPosition();
-    }
+                      SnookerSimulation *simulation);
+    virtual ~PlaceCueBallState();
 
   protected:
-    virtual IState *testTransferFrom() const
-    {
-      // Move to take shot when ball is placed
-      if (m_completed)
-        return m_parent->findState("take_shot").back();
-      else
-        return nullptr;
-    }
-
-    virtual void onEntry(IState *last)
-    {
-      // Reset recorded mouse start position
-      resetMouseStartPosition();
-    }
-
-    virtual void onOperate()
-    {
-      if (m_simulation->menu->isMouseOver())
-        return;
-
-      // Handle controls for placing cue ball
-      if (m_mouseStartPosition == nullptr)
-      {
-        if (m_simulation->controls->state(S_TAKE_SHOT))
-        {
-          // Record starting position of mouse
-          m_mouseStartPosition = new Engine::Maths::Vector2(m_simulation->controls->analog(A_MOUSE_X),
-                                                            m_simulation->controls->analog(A_MOUSE_Y));
-        }
-      }
-      else
-      {
-        Engine::Maths::Vector2 newMousePosition = Engine::Maths::Vector2(m_simulation->controls->analog(A_MOUSE_X),
-                                                                         m_simulation->controls->analog(A_MOUSE_Y));
-        Engine::Maths::Vector2 deltaMouse = *m_mouseStartPosition - newMousePosition;
-
-        // Clamp max acceleration to a sensible level
-        float maxShotMagnitude = 0.5f;
-        if (deltaMouse.length2() > (maxShotMagnitude * maxShotMagnitude))
-          deltaMouse = Engine::Maths::VectorOperations::GetNormalised(deltaMouse) * maxShotMagnitude;
-
-        if (!m_simulation->controls->state(S_TAKE_SHOT))
-        {
-          resetMouseStartPosition();
-          m_completed = true;
-        }
-        else
-        {
-          m_simulation->balls[0]->setPosition(newMousePosition * 2000.0f);
-        }
-      }
-    }
+    virtual IState *testTransferFrom() const;
+    virtual void onEntry(IState *last);
+    virtual void onOperate();
 
   private:
     inline void resetMouseStartPosition()
--- Simulation_Snooker/TakeShotState.h
+++ Simulation_Snooker/TakeShotState.h
@@ -27,105 +27,14 @@ namespace Snooker
   class TakeShotState : public Simulation::AI::CompletableActionState
   {
   public:
-    TakeShotState(Simulation::AI::IState *parent, Simulation::AI::StateMachine *machine, SnookerSimulation *simulation)
-        : CompletableActionState("take_shot", parent, machine)
-        , m_simulation(simulation)
-    {
-    }
-
-    virtual ~TakeShotState()
-    {
-      resetMouseStartPosition();
-    }
+    TakeShotState(Simulation::AI::IState *parent, Simulation::AI::StateMachine *machine, SnookerSimulation *simulation);
+    virtual ~TakeShotState();
 
   protected:
-    virtual IState *testTransferFrom() const
-    {
-      if (m_completed)
-        return m_parent->findState("wait_for_shot").back();
-      else
-        return nullptr;
-    }
-
-    virtual void onExit(IState *next)
-    {
-      (void)next;
-      m_simulation->balls[0]->setAcceleration(Engine::Maths::Vector2());
-    }
-
-    virtual void onEntry(IState *last)
-    {
-      CompletableActionState::onEntry(last);
-      resetMouseStartPosition();
-
-      WaitForShotState *shotState = dynamic_cast<WaitForShotState *>(m_parent->findState("wait_for_shot").back());
-      if (shotState == nullptr)
-        return;
-
-      // If players last shot in turn was a red then now they hit any colour
-      if (shotState->lastPotted() != nullptr && shotState->lastPotted()->points() == 1)
-      {
-        shotState->setTargetBallPoints(0);
-        return;
-      }
-
-      // Players first shot in turn is either on red or the next colour in sequence if no reds are in play
-      // i.e. whatever the lowest value ball in play is
-      for (size_t i = 1; i < SnookerSimulation::NUM_BALLS; i++)
-      {
-        if (m_simulation->balls[i]->collides() && shotState)
-        {
-          shotState->setTargetBallPoints(m_simulation->balls[i]->points());
-          return;
-        }
-      }
-    }
-
-    virtual void onOperate()
-    {
-      if (m_simulation->menu->isMouseOver())
-        return;
-
-      // Mouse clicks (to take shots)
-      if (m_mouseStartPosition == nullptr)
-      {
-        if (m_simulation->controls->state(S_TAKE_SHOT))
-        {
-          // Record starting position of mouse
-          m_mouseStartPosition = new Engine::Maths::Vector2(m_simulation->controls->analog(A_MOUSE_X),
-                                                            m_simulation->controls->analog(A_MOUSE_Y));
-          static_cast<Engine::Graphics::LineMesh *>(m_simulation->shotAimLine->mesh())->setTo(Engine::Maths::Vector3());
-          m_simulation->shotAimLine->setActive(true);
-        }
-        else
-        {
-          m_simulation->balls[0]->setAcceleration(Engine::Maths::Vector2());
-        }
-      }
-      else
-      {
-        Engine::Maths::Vector2 newMousePosition = Engine::Maths::Vector2(m_simulation->controls->analog(A_MOUSE_X),
-                                                                         m_simulation->controls->analog(A_MOUSE_Y));
-        Engine::Maths::Vector2 deltaMouse = *m_mouseStartPosition - newMousePosition;
-
-        // Clamp max acceleration to a sensible level
-        float maxShotMagnitude = 0.5f;
-        if (deltaMouse.length2() > (maxShotMagnitude * maxShotMagnitude))
-          deltaMouse = Engine::Maths::VectorOperations::GetNormalised(deltaMouse) * maxShotMagnitude;
-
-        if (!m_simulation->controls->state(S_TAKE_SHOT))
-        {
-          m_simulation->shotAimLine->setActive(false);
-          m_simulation->balls[0]->setAcceleration(deltaMouse);
-          resetMouseStartPosition();
-          m_completed = true;
-        }
-        else
-        {
-          static_cast<Engine::Graphics::LineMesh *>(m_simulation->shotAimLine->mesh())->setTo(deltaMouse * 1000.0f);
-        }
-      }
-    }
+    virtual IState *testTransferFrom() const;
+    virtual void onExit(IState *next);
+    virtual void onEntry(IState *last);
+    virtual void onOperate();
 
   private:
     inline void resetMouseStartPosition()
