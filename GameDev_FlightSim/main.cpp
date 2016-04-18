f6ac281c28da7600c15c7f7369f4b85a6aee68c4
Dan Nixon <dan@dan-nixon.com>
2016-04-18 21:21:46 +0100

    add empty project for game dev courseowrk 2

---
+++ GameDev_FlightSim/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @author Dan Nixon (120263697)
+ *
+ * For CSC3224 Project 2.
+ */
+
+#include <iostream>
+
+/**
+ * @brief Entry point of flight sim game.
+ */
+int main(int argc, char *args[])
+{
+  std::cout << "todo" << std::endl;
+  return 0;
+}
