cda83ab2cbdefa9a759dcfb68013f87e236f128a
Dan Nixon <dan@dan-nixon.com>
2016-04-26 15:59:11 +0100

    add tests for reverse paths

--- Simulation_PathFinding_Test/AStarTest.cpp
+++ Simulation_PathFinding_Test/AStarTest.cpp
@@ -58,7 +58,7 @@ public:
     Assert::IsTrue(nodes[3] == path[0]);
   }
 
-  TEST_METHOD(AStarTest_TestPath_1)
+  TEST_METHOD(AStarTest_TestPath_1_fwd)
   {
     // Load test graph
     std::vector<Node *> nodes;
@@ -76,8 +76,27 @@ public:
     Assert::IsTrue(nodes[4] == path[1]);
     Assert::IsTrue(nodes[5] == path[2]);
   }
+  
+  TEST_METHOD(AStarTest_TestPath_1_rev)
+  {
+    // Load test graph
+    std::vector<Node *> nodes;
+    std::vector<Edge *> edges;
+    GraphLoader::LoadGraph(nodes, edges, "../../../../resources/test_graph.dat");
 
-  TEST_METHOD(AStarTest_TestPath_2)
+    // Run path finding
+    AStar pathFinder(nodes);
+    Assert::IsTrue(pathFinder.findPath(nodes[5], nodes[3]));
+
+    // Assert path
+    std::vector<Node *> &path = pathFinder.path();
+    Assert::AreEqual((size_t)3, path.size());
+    Assert::IsTrue(nodes[5] == path[0]);
+    Assert::IsTrue(nodes[4] == path[1]);
+    Assert::IsTrue(nodes[3] == path[2]);
+  }
+
+  TEST_METHOD(AStarTest_TestPath_2_fwd)
   {
     // Load test graph
     std::vector<Node *> nodes;
@@ -95,8 +114,27 @@ public:
     Assert::IsTrue(nodes[3] == path[1]);
     Assert::IsTrue(nodes[1] == path[2]);
   }
+  
+  TEST_METHOD(AStarTest_TestPath_2_rev)
+  {
+    // Load test graph
+    std::vector<Node *> nodes;
+    std::vector<Edge *> edges;
+    GraphLoader::LoadGraph(nodes, edges, "../../../../resources/test_graph.dat");
 
-  TEST_METHOD(AStarTest_TestPath_3)
+    // Run path finding
+    AStar pathFinder(nodes);
+    Assert::IsTrue(pathFinder.findPath(nodes[1], nodes[6]));
+
+    // Assert path
+    std::vector<Node *> &path = pathFinder.path();
+    Assert::AreEqual((size_t)3, path.size());
+    Assert::IsTrue(nodes[1] == path[0]);
+    Assert::IsTrue(nodes[3] == path[1]);
+    Assert::IsTrue(nodes[6] == path[2]);
+  }
+
+  TEST_METHOD(AStarTest_TestPath_3_fwd)
   {
     // Load test graph
     std::vector<Node *> nodes;
@@ -114,8 +152,27 @@ public:
     Assert::IsTrue(nodes[4] == path[1]);
     Assert::IsTrue(nodes[0] == path[2]);
   }
+  
+  TEST_METHOD(AStarTest_TestPath_3_rev)
+  {
+    // Load test graph
+    std::vector<Node *> nodes;
+    std::vector<Edge *> edges;
+    GraphLoader::LoadGraph(nodes, edges, "../../../../resources/test_graph.dat");
 
-  TEST_METHOD(AStarTest_TestPath_4)
+    // Run path finding
+    AStar pathFinder(nodes);
+    Assert::IsTrue(pathFinder.findPath(nodes[0], nodes[8]));
+
+    // Assert path
+    std::vector<Node *> &path = pathFinder.path();
+    Assert::AreEqual((size_t)3, path.size());
+    Assert::IsTrue(nodes[0] == path[0]);
+    Assert::IsTrue(nodes[4] == path[1]);
+    Assert::IsTrue(nodes[8] == path[2]);
+  }
+
+  TEST_METHOD(AStarTest_TestPath_4_fwd)
   {
     // Load test graph
     std::vector<Node *> nodes;
@@ -131,6 +188,24 @@ public:
     Assert::AreEqual((size_t)2, path.size());
     Assert::IsTrue(nodes[7] == path[0]);
     Assert::IsTrue(nodes[4] == path[1]);
+  }
+  
+  TEST_METHOD(AStarTest_TestPath_4_rev)
+  {
+    // Load test graph
+    std::vector<Node *> nodes;
+    std::vector<Edge *> edges;
+    GraphLoader::LoadGraph(nodes, edges, "../../../../resources/test_graph.dat");
+
+    // Run path finding
+    AStar pathFinder(nodes);
+    Assert::IsTrue(pathFinder.findPath(nodes[4], nodes[7]));
+
+    // Assert path
+    std::vector<Node *> &path = pathFinder.path();
+    Assert::AreEqual((size_t)2, path.size());
+    Assert::IsTrue(nodes[4] == path[0]);
+    Assert::IsTrue(nodes[7] == path[1]);
   }
 };
 #endif /* DOXYGEN_SKIP */
